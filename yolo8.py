39033bed23d06d09fd15dcece1f7e8e18b17d19a
main <bofanh3@gmail.com>
2024-05-08 22:10:43 +0000

    change data path

--- yolo8.py
+++ yolo8.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
 
 # Train the model
-results = model.train(data='subcell6/dataset', epochs=50, imgsz=128)
+results = model.train(data='subcell7/dataset', epochs=50, imgsz=128)
